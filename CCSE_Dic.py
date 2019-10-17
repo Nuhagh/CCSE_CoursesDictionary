fd0684fcc56aed4934702e19698f958b619337b0
Nuhagh <53125040+Nuhagh@users.noreply.github.com>
2019-10-18 01:59:31 +0300

    added comments

--- CCSE_Dic.py
+++ CCSE_Dic.py
@@ -1,11 +1,27 @@
+#import json library
 import json
 
+#load dictionary from json file
 courses=json.load(open("CCSE_Courses.json"))
-x="y"
-while(x=="y"):
+
+#check if the user want to repeat the search
+repeat="y"
+
+#if yes
+while(repeat=="y"):
+    #get the code and the number of the course, remove spaces from left,right and in the middle
+    #make all the input in lower case
     user_input=input("Enter The course code and number:").replace(" ","").strip().lower()
+    
+    #if user enters an existed course
     if user_input in courses.keys():
+        #Course name and department is printed here
         print(courses[user_input])
+    #if user enters a wrong course name
     else:
         print("No Course Found, check the code & the number")
-    x=input("Search Another Course?(y/n)")
+   
+    #Ask user if he wants to re-enter another course name   
+    repeat=input("Search Another Course? Press y for yes or n for no)")
+    if repeat != "n" and repeat !="y":
+        x=input("PLEASE. Press y for yes or n for no)")
